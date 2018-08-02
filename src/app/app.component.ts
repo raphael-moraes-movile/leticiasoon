6eda1a959a095141b1cadf7ae8922712a5bca9c6
Raphael Moraes <raphael.moraes@movile.com>
2018-08-02 01:15:25 -0300

    Initial site for Leticia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leticia-soon';
+
+  openWhatsapp() {
+    window.open(
+      // tslint:disable-next-line:max-line-length
+      'https://api.whatsapp.com/send?phone=5511972916705&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20horario,%20por%20favor&source=&data='
+    );
+  }
 }
